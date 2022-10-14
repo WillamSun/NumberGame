bec4cdb3e6cc4ff804bd507b7a9b6f7a9abd5c54
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2022-10-14 12:05:41 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace NumberGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 90);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(568, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "换一个";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F);
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(519, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "      +       +        +        +        +   ;    +";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("宋体", 19F);
+            this.label2.Location = new System.Drawing.Point(4, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 26);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "(0)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(567, 74);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "数字";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("宋体", 19F);
+            this.label7.Location = new System.Drawing.Point(439, 24);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 26);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "(0)";
+            this.label7.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("宋体", 19F);
+            this.label6.Location = new System.Drawing.Point(353, 24);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 26);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "(0)";
+            this.label6.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("宋体", 19F);
+            this.label5.Location = new System.Drawing.Point(262, 22);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 26);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "(0)";
+            this.label5.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("宋体", 19F);
+            this.label4.Location = new System.Drawing.Point(172, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 26);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "(0)";
+            this.label4.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("宋体", 19F);
+            this.label3.Location = new System.Drawing.Point(86, 22);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 26);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "(0)";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("宋体", 19F);
+            this.label8.Location = new System.Drawing.Point(516, 24);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 26);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "(0)";
+            this.label8.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(10, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(567, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Location = new System.Drawing.Point(583, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(125, 129);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "分数";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("宋体", 20F);
+            this.label9.Location = new System.Drawing.Point(6, 17);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(113, 109);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "0";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(720, 148);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "数字抵消";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace NumberGame
+{
+    public partial class Form1 : Form
+    {
+        int Score = 0;
+        int SelectItems = -114514;
+        List<Label> SelectItemsList = new List<Label> { };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            foreach (Label label in new List<Label> {label2,label3,label4,label5,label6,label7,label8})
+            {
+                label.BorderStyle = BorderStyle.None;
+                label.Text = RandomLabelText();
+                Thread.Sleep(10);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            if (label.BorderStyle != BorderStyle.FixedSingle)
+            {
+                if (SelectItems == -114514)
+                {
+                    SelectItems = 0;
+                }
+                SelectItems += int.Parse((label).Text.Replace("(", string.Empty).Replace(")", string.Empty));
+                SelectItemsList.Add(label);
+                label.BorderStyle = BorderStyle.FixedSingle;
+                return;
+            }
+            SelectItems -= int.Parse((label).Text.Replace("(", string.Empty).Replace(")", string.Empty));
+            SelectItemsList.Remove(label);
+            label.BorderStyle = BorderStyle.None;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (SelectItems == -114514)
+            {
+                foreach (Label label in new List<Label> { label2, label3, label4, label5, label6, label7, label8 })
+                {
+                    label.BorderStyle = BorderStyle.None;
+                    label.Text = RandomLabelText();
+                    Thread.Sleep(10);
+                }
+                Score = 0;
+                return;
+            }
+            foreach (Label label in SelectItemsList)
+            {
+                label.Text = RandomLabelText();
+                label.BorderStyle = BorderStyle.None;
+                Thread.Sleep(10);
+            }
+            Score++;
+            SelectItemsList.Clear();
+            SelectItems = -114514;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (SelectItemsList.Count == 0) SelectItems = -114514;
+            if (SelectItems == -114514) button1.Text = "重开一局";
+            else button1.Text = "抵消";
+            if (SelectItems == 0 || SelectItems == -114514) button1.Enabled = true;
+            else button1.Enabled = false;
+            label9.Text = Score.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private string RandomLabelText()
+        {
+            int integer = new Random().Next(-9, 9);
+            if (integer < 0)
+            {
+                return "(" + integer + ")";
+            }
+            else if (integer == 0)
+            {
+                return "(0)";
+            }
+            else
+            {
+                return "(+" + integer + ")";
+            }
+        }
+    }
+}
