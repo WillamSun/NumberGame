0041d6e93f4c8530b5e6a0c8782a636eb69901d8
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2022-12-30 21:01:12 +0800

    添加了输的条件，增加了难度

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,18 +31,19 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.LinkLabel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.LinkLabel();
+            this.label7 = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.LinkLabel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -53,7 +54,7 @@
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(757, 29);
-            this.button1.TabIndex = 10;
+            this.button1.TabIndex = 7;
             this.button1.Text = "换一个";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -73,10 +74,13 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("宋体", 19F);
+            this.label2.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.label2.LinkColor = System.Drawing.Color.Black;
             this.label2.Location = new System.Drawing.Point(5, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 33);
-            this.label2.TabIndex = 11;
+            this.label2.TabIndex = 0;
+            this.label2.TabStop = true;
             this.label2.Text = "(0)";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -104,10 +108,13 @@
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("宋体", 19F);
+            this.label8.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.label8.LinkColor = System.Drawing.Color.Black;
             this.label8.Location = new System.Drawing.Point(688, 30);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(63, 33);
-            this.label8.TabIndex = 17;
+            this.label8.TabIndex = 6;
+            this.label8.TabStop = true;
             this.label8.Text = "(0)";
             this.label8.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -116,10 +123,13 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("宋体", 19F);
+            this.label7.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.label7.LinkColor = System.Drawing.Color.Black;
             this.label7.Location = new System.Drawing.Point(585, 30);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(63, 33);
-            this.label7.TabIndex = 16;
+            this.label7.TabIndex = 5;
+            this.label7.TabStop = true;
             this.label7.Text = "(0)";
             this.label7.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -128,10 +138,13 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("宋体", 19F);
+            this.label6.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.label6.LinkColor = System.Drawing.Color.Black;
             this.label6.Location = new System.Drawing.Point(471, 30);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 33);
-            this.label6.TabIndex = 15;
+            this.label6.TabIndex = 4;
+            this.label6.TabStop = true;
             this.label6.Text = "(0)";
             this.label6.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -140,10 +153,13 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("宋体", 19F);
+            this.label5.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.label5.LinkColor = System.Drawing.Color.Black;
             this.label5.Location = new System.Drawing.Point(349, 28);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 33);
-            this.label5.TabIndex = 14;
+            this.label5.TabIndex = 3;
+            this.label5.TabStop = true;
             this.label5.Text = "(0)";
             this.label5.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -152,10 +168,13 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("宋体", 19F);
+            this.label4.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.label4.LinkColor = System.Drawing.Color.Black;
             this.label4.Location = new System.Drawing.Point(229, 28);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(63, 33);
-            this.label4.TabIndex = 13;
+            this.label4.TabIndex = 2;
+            this.label4.TabStop = true;
             this.label4.Text = "(0)";
             this.label4.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -164,10 +183,13 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("宋体", 19F);
+            this.label3.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.label3.LinkColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(115, 28);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 33);
-            this.label3.TabIndex = 12;
+            this.label3.TabIndex = 1;
+            this.label3.TabStop = true;
             this.label3.Text = "(0)";
             this.label3.Click += new System.EventHandler(this.label2_Click);
             // 
@@ -180,10 +202,10 @@
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button2.Location = new System.Drawing.Point(13, 148);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(756, 29);
-            this.button2.TabIndex = 11;
+            this.button2.TabIndex = 8;
             this.button2.Text = "退出";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -192,9 +214,9 @@
             // 
             this.groupBox2.Controls.Add(this.label9);
             this.groupBox2.Location = new System.Drawing.Point(777, 15);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox2.Size = new System.Drawing.Size(167, 161);
             this.groupBox2.TabIndex = 12;
             this.groupBox2.TabStop = false;
@@ -211,12 +233,26 @@
             this.label9.Text = "0";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(960, 185);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "你输了\r\n点击重开一局";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button2;
             this.ClientSize = new System.Drawing.Size(960, 185);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -241,18 +277,19 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel label3;
+        private System.Windows.Forms.LinkLabel label6;
+        private System.Windows.Forms.LinkLabel label5;
+        private System.Windows.Forms.LinkLabel label4;
+        private System.Windows.Forms.LinkLabel label7;
+        private System.Windows.Forms.LinkLabel label8;
+        private System.Windows.Forms.LinkLabel label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,10 +8,12 @@ namespace NumberGame
 {
     public partial class Form1 : Form
     {
-        int Help = 0;
-        int Score = 0;
-        int SelectItems = -114514;
-        List<Label> SelectItemsList = new List<Label> { };
+        string SelectItemsString;
+        int Help;
+        int Score;
+        int SelectItems;
+        List<LinkLabel> SelectItemsList;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,14 +21,20 @@ namespace NumberGame
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            SelectItemsList = new List<LinkLabel>();
+            Score = 0;
+            Help = 0;
+            SelectItemsString = "";
+            SelectItems = -114514;
             timer1.Start();
             InitializeLabels();
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-            if (!(sender is Label)) return;
-            Label label = sender as Label;
+            if (!(sender is LinkLabel)) return;
+            LinkLabel label = sender as LinkLabel;
+            SelectItemsString = "";
             if (label.BorderStyle != BorderStyle.FixedSingle)
             {
                 if (SelectItems == -114514)
@@ -36,27 +44,66 @@ namespace NumberGame
                 SelectItems += int.Parse(label.Text.Replace("(", string.Empty).Replace(")", string.Empty));
                 SelectItemsList.Add(label);
                 label.BorderStyle = BorderStyle.FixedSingle;
+                foreach (LinkLabel l in SelectItemsList)
+                {
+                    if (SelectItemsList.Last() == l) SelectItemsString += l.Text;
+                    else SelectItemsString += l.Text + " + ";
+                }
                 return;
             }
             SelectItems -= int.Parse(label.Text.Replace("(", string.Empty).Replace(")", string.Empty));
             SelectItemsList.Remove(label);
             label.BorderStyle = BorderStyle.None;
+            foreach (LinkLabel l in SelectItemsList)
+            {
+                if (SelectItemsList.Last() == l) SelectItemsString += l.Text;
+                else SelectItemsString += l.Text + " + ";
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (button3.Visible == true)
+            {
+                MessageBox.Show("都输了还想作弊","提示",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
             if (SelectItems == -114514)
             {
                 if (GetOKOffsetLabels().Count != 0)
                 {
-                    MessageBox.Show("还有可以抵消的数", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    Score--;
+                    string text = "";
+                    List<List<LinkLabel>> l = GetOKOffsetLabels();
+                    foreach (List<LinkLabel> ls in l)
+                    {
+                        foreach (LinkLabel label in ls)
+                        {
+                            if (ls.IndexOf(label) == ls.Count - 1)
+                            {
+                                text += label.Text + "\n";
+                            }
+                            else
+                            {
+                                text += label.Text + " + ";
+                            }
+                        }
+                    }
+                    MessageBox.Show("还有可以抵消的数，可行的选择方案: \n" + text + "\n游戏结束", "你输了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    button3.Visible = true;
+                    button3.Focus();
                     return;
                 }
                 InitializeLabels();
                 return;
             }
-            foreach (Label label in SelectItemsList)
+            if (SelectItems != 0)
+            {
+                MessageBox.Show(SelectItemsString + " = " + SelectItems + " ≠ 0" + "\n游戏结束", "你输了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button3.Visible = true;
+                button3.Focus();
+                return;
+            }
+            foreach (LinkLabel label in SelectItemsList)
             {
                 label.Text = RandomLabelText();
                 label.BorderStyle = BorderStyle.None;
@@ -72,8 +119,6 @@ namespace NumberGame
             if (SelectItemsList.Count == 0) SelectItems = -114514;
             if (SelectItems == -114514) button1.Text = "检测是否死局";
             else button1.Text = "抵消";
-            if (SelectItems == 0 || SelectItems == -114514) button1.Enabled = true;
-            else button1.Enabled = false;
             label9.Text = Score.ToString();
         }
 
@@ -101,7 +146,7 @@ namespace NumberGame
 
         private void InitializeLabels()
         {
-            foreach (Label label in Labels)
+            foreach (LinkLabel label in Labels)
             {
                 label.BorderStyle = BorderStyle.None;
                 label.Text = RandomLabelText();
@@ -109,17 +154,17 @@ namespace NumberGame
             }
         }
 
-        private List<List<Label>> GetOKOffsetLabels()
+        private List<List<LinkLabel>> GetOKOffsetLabels()
         {
-            List<List<Label>> result = new List<List<Label>>();
-            Label[] labels = Labels;
+            List<List<LinkLabel>> result = new List<List<LinkLabel>>();
+            LinkLabel[] labels = Labels;
             for (int i = 1; i < labels.Length; i++) //选择数
             {
                 List<List<int>> list = new List<List<int>>();
                 list.AddRange(PermutationCombinations(Enumerable.Range(0,labels.Length).ToList(),i));
                 foreach (List<int> ls in list)
                 {
-                    List<Label> tmp = new List<Label>();
+                    List<LinkLabel> tmp = new List<LinkLabel>();
                     int integer = 0;
                     foreach (int inte in ls)
                     {
@@ -138,24 +183,73 @@ namespace NumberGame
             return result;
         }
 
+        int openHelpMenu = 0;
         private void Form1_HelpButtonClicked(object sender, System.ComponentModel.CancelEventArgs e)
         {
             e.Cancel = true;
-            if (MessageBox.Show("玩法：\n" +
-                "打开软件后可以看到左上方的横式，右上方的分数，下方有两个按钮\n" +
-                "选择一些加起来等于0的数字（单独一个0也可以，或者-3，-2，+5这三个数也可以，不限项数）\n" +
-                "选择好后，下方的“检测是否死局”按钮会变成“抵消”\n" +
-                "如果所有选择的数相加等于0，按下“抵消”按钮即可得1分\n" +
-                "如果觉得陷入了死局，可以按下“检测是否死局”，如确实死局，可以重开一次，如没有，将会扣掉一分\n\n" +
-                "陷入迷茫？点击“是”获取提示\n" +
-                "已使用" + Help + "次提示", "帮助", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            openHelpMenu++;
+            string HelpText = "玩法：\n" +
+                    "打开软件后可以看到左上方的横式，右上方的分数，下方有两个按钮\n" +
+                    "选择一些加起来等于0的数字（单独一个0也可以，或者-3，-2，+5这三个数也可以，不限项数）\n" +
+                    "选择好后，下方的“检测是否死局”按钮会变成“抵消”\n" +
+                    "如果所有选择的数相加等于0，按下“抵消”按钮即可得1分，如果选错了会结束游戏\n" +
+                    "如果觉得陷入了死局，可以按下“检测是否死局”，如确实死局，可以重新生成一次，如没有，将会结束游戏" +
+                    ((Help >= 3) ? ("\n\n已使用" + Help + "次提示，不可再次使用") : ("\n\n陷入迷茫？点击“是”获取提示\n" + "已使用" + Help + "次提示，为了游戏体验，只可使用3次提示，你还剩" + (3 - Help) + "次机会"));
+            DialogResult dialogResult;
+            switch (openHelpMenu)
+            {
+                case 5:
+                    dialogResult = MessageBox.Show("这个菜单好看吗\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                case 6:
+                    dialogResult = MessageBox.Show("别看了，有啥看头?\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                case 7:
+                    dialogResult = MessageBox.Show("算了你看吧\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                case 8:
+                    dialogResult = MessageBox.Show("...\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                case 9:
+                    dialogResult = MessageBox.Show("真就继续看呗!\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                case 10:
+                    dialogResult = MessageBox.Show("既然你这么喜欢这个菜单,就去GitHub给这个项目点个Star呗~\n点击“帮助”前往GitHub项目地址\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2,0,"https://github.com/WillamSun/NumberGame");
+                    break;
+                case 11:
+                    dialogResult = MessageBox.Show("行行，我坦白，我确实sha人了\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Error, MessageBoxDefaultButton.Button2);
+                    break;
+                case 12:
+                case 13:
+                case 14:
+                    dialogResult = MessageBox.Show("6\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                case 15:
+                case 16:
+                case 17:
+                    dialogResult = MessageBox.Show("大哥佩服\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                case 18:
+                case 19:
+                    dialogResult = MessageBox.Show("爸爸\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+                default:
+                    if (openHelpMenu >= 20)
+                    {
+                        dialogResult = MessageBox.Show("傻逼别看了\n\n" + HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+                        break;
+                    }
+                    dialogResult = MessageBox.Show(HelpText, "帮助", (Help >= 3) ? MessageBoxButtons.OK : MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+                    break;
+            }
+            if (dialogResult == DialogResult.Yes)
             {
                 Help++;
                 string text = "";
-                List<List<Label>> l = GetOKOffsetLabels();
-                foreach (List<Label> ls in l)
+                List<List<LinkLabel>> l = GetOKOffsetLabels();
+                foreach (List<LinkLabel> ls in l)
                 {
-                    foreach (Label label in ls)
+                    foreach (LinkLabel label in ls)
                     {
                         if (ls.IndexOf(label) == ls.Count - 1)
                         {
@@ -172,15 +266,15 @@ namespace NumberGame
             }
         }
 
-        private Label[] Labels
+        private LinkLabel[] Labels
         {
             get
             {
-                List<Label> labels = new List<Label>();
+                List<LinkLabel> labels = new List<LinkLabel>();
                 foreach (Control control in groupBox1.Controls)
                 {
-                    if (!(control is Label) || control.Name == "label1") continue;
-                    labels.Add(control as Label);
+                    if (!(control is LinkLabel)) continue;
+                    labels.Add(control as LinkLabel);
                 }
                 return labels.ToArray();
             }
@@ -229,6 +323,13 @@ namespace NumberGame
                 }
             }
             return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Controls.Clear();
+            InitializeComponent();
+            Form1_Load(sender, e);
         }
     }
 }
